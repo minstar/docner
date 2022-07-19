93611e84b022f3c6d19aee2555bf77eb20d08a1d
minstar <wjdalsquf@gmail.com>
2022-07-19 05:25:43 +0000

    get prediction statistics update

--- preprocess/dataset_attribute.py
+++ preprocess/dataset_attribute.py
@@ -30,7 +30,7 @@ import numpy as np
 
 from tqdm import tqdm, trange
 
-def token_frequency(token_freq_dict, token_cons_dict, words, labels):
+def token_frequency(token_freq_dict, token_cons_dict, words, labels, entity_name):
     temp_token_cons_dict = {}
     for word in words:
         if word not in token_freq_dict:
@@ -51,26 +51,37 @@ def token_frequency(token_freq_dict, token_cons_dict, words, labels):
         for label_id, label_cnt in val.items():
             total_cnt += label_cnt
 
-        temp_cnt, temp_cnt2 = 0, 0
-        for label_id, label_cnt in val.items():
-            if label_id == 0:
-                temp_list.append(label_cnt / total_cnt)
-            elif label_id == 1:
-                temp_cnt += label_cnt
-            elif label_id == 2:
-                temp_cnt += label_cnt
-                temp_list.append(temp_cnt / total_cnt)
-            elif label_id == 3:
-                temp_cnt2 += label_cnt
-            elif label_id == 4:
-                temp_cnt2 += label_cnt
-                temp_list.append(temp_cnt2 / total_cnt)
+        if 'bc5cdr' == entity_name:
+            temp_cnt, temp_cnt2 = 0, 0
+            for label_id, label_cnt in val.items():
+                if label_id == 0:
+                    temp_list.append(label_cnt / total_cnt)
+                elif label_id == 1:
+                    temp_cnt += label_cnt
+                elif label_id == 2:
+                    temp_cnt2 += label_cnt
+                elif label_id == 3:
+                    temp_cnt += label_cnt
+                    temp_list.append(temp_cnt / total_cnt)
+                elif label_id == 4:
+                    temp_cnt2 += label_cnt
+                    temp_list.append(temp_cnt2 / total_cnt)
+        else:
+            temp_cnt, temp_cnt2 = 0, 0
+            for label_id, label_cnt in val.items():
+                if label_id == 0:
+                    temp_list.append(label_cnt / total_cnt)
+                elif label_id == 1:
+                    temp_cnt += label_cnt
+                elif label_id == 2:
+                    temp_cnt += label_cnt
+                    temp_list.append(temp_cnt / total_cnt)
             
         token_cons_dict[key].append(temp_list)
 
     return token_freq_dict, token_cons_dict
 
-def entity_frequency(entity_freq_dict, entity_density_list, entity_cons_dict, words, labels):
+def entity_frequency(entity_freq_dict, entity_density_list, entity_cons_dict, words, labels, entity_name):
     entity = ""
     in_entity = ""
     in_entity_freq_dict = {}
@@ -84,15 +95,22 @@ def entity_frequency(entity_freq_dict, entity_density_list, entity_cons_dict, wo
 
     for idx, (word, label) in enumerate(zip(words, labels)):
         if label != 0:
-            if label % 2 == 1:
+            if label in [1, 2]:
                 entity += word + " "
                 in_entity += word + " "
                 try:
-                    if labels[idx+1] == 0 or labels[idx+1] == 1:
-                        entity_freq_dict, entity = cnt_function(entity_freq_dict, entity)
-                        in_entity_freq_dict, in_entity = cnt_function(in_entity_freq_dict, in_entity)
+                    if 'bc5cdr' == entity_name:
+                        if labels[idx+1] == 0 or labels[idx+1] == 1 or labels[idx+1] == 2:
+                            entity_freq_dict, entity = cnt_function(entity_freq_dict, entity)
+                            in_entity_freq_dict, in_entity = cnt_function(in_entity_freq_dict, in_entity)
+                        else:
+                            continue
                     else:
-                        continue
+                        if labels[idx+1] == 0 or labels[idx+1] == 1:
+                            entity_freq_dict, entity = cnt_function(entity_freq_dict, entity)
+                            in_entity_freq_dict, in_entity = cnt_function(in_entity_freq_dict, in_entity)
+                        else:
+                            continue
                 except:
                     entity_freq_dict, entity = cnt_function(entity_freq_dict, entity)
                     in_entity_freq_dict, in_entity = cnt_function(in_entity_freq_dict, in_entity)
@@ -102,7 +120,7 @@ def entity_frequency(entity_freq_dict, entity_density_list, entity_cons_dict, wo
                 try:
                     if labels[idx+1] != 0:
                         continue
-                    else:
+                    else:                
                         entity_freq_dict, entity = cnt_function(entity_freq_dict, entity)
                         in_entity_freq_dict, in_entity = cnt_function(in_entity_freq_dict, in_entity)
                 except:
@@ -193,14 +211,66 @@ def length_per_cons(train_entity_freq_dict, train_entity_cons_dict):
 #     return dens_cons_dict
 
 def main():
-    data_dir = '/ssd1/minbyul/docner/data/low-resource'
-    entity_list = ['ncbi-disease', 'bc5cdr', 'anatem', 'gellus']
-    file_list = ['doc_train.json', 'doc_dev.json', 'doc_test.json']
-    # entity_name = 'ncbi-disease' 
+    # data_dir = '/ssd1/minbyul/docner/data/low-resource'
+    # entity_list = ['ncbi-disease', 'bc5cdr', 'anatem', 'gellus']
+    # file_list = ['doc_train.json', 'doc_dev.json', 'doc_test.json']
+    # # entity_name = 'ncbi-disease' 
+    # for entity_name in entity_list:
+    #     out_of_dens_dict = {}
+    #     for file_name in file_list:
+    #         with open(data_dir+'/'+entity_name+'/'+'from_rawdata'+'/'+file_name, 'r') as fp:
+    #             data = json.load(fp)
+    #             token_freq_dict, entity_freq_dict = {}, {}
+    #             token_cons_dict, entity_cons_dict = {}, {}
+    #             doc_len,entity_density_list = [], []
+
+    #             for data_idx, data_inst in tqdm(enumerate(data), desc='Total Run'):
+    #                 words = data_inst['str_words']
+    #                 labels = data_inst['tags']
+                    
+    #                 token_freq_dict, token_cons_dict = token_frequency(token_freq_dict, token_cons_dict, words, labels)
+    #                 entity_freq_dict, entity_density_list, entity_cons_dict = entity_frequency(entity_freq_dict, entity_density_list, entity_cons_dict, words, labels, entity_name)
+    #                 doc_len = document_length(doc_len, words)
+
+    #             entity_len_dict = entity_length(entity_freq_dict)
+
+    #         if 'train' in file_name:
+    #             train_entity_freq_dict = copy.deepcopy(entity_freq_dict)
+    #             train_entity_cons_dict = copy.deepcopy(entity_cons_dict)
+    #             # train_entity_density_list = copy.deepcopy(entity_density_list)
+
+    #         if 'test' in file_name:
+    #             test_data = copy.deepcopy(data)
+    #             test_entity_freq_dict = copy.deepcopy(entity_freq_dict)
+    #             test_doc_len = copy.deepcopy(doc_len)
+
+    #     length_cons_dict = length_per_cons(train_entity_freq_dict, train_entity_cons_dict)
+
+        # dens_cons_dict = dens_per_cons(train_entity_freq_dict, train_entity_density_list)
+
+        # get out of density through a set of training entites 
+        # out_of_dens_dict = out_of_density(train_entity_freq_dict, test_entity_freq_dict, out_of_dens_dict, test_doc_len, test_data)
+        # get_list = []
+        # for key, val_list in out_of_dens_dict.items():
+        #     get_list.append(np.mean(val_list))
+
+        # print (np.mean(get_list), np.std(get_list))
+
+    #####################################################################################################
+    data_dir = '/ssd1/minbyul/docner/models/fine-tuned'
+    entity_list = ['anatem']
+    file_list = ['preds_doc_test.json']
+    models="biolm-large"
+    EP="30"
+    LR="5e-5"
+    ML="512"
+    WM="0"
+    DATA_TYPE="doc"
+    
     for entity_name in entity_list:
         out_of_dens_dict = {}
         for file_name in file_list:
-            with open(data_dir+'/'+entity_name+'/'+'from_rawdata'+'/'+file_name, 'r') as fp:
+            with open(data_dir+'/'+entity_name+'/'+models+"_"+"EP%s_"%EP+"LR%s_"%LR+"ML%s_"%ML+"WM%s_"%WM+DATA_TYPE+'/'+"checkpoint-best/"+file_name, 'r') as fp:
                 data = json.load(fp)
                 token_freq_dict, entity_freq_dict = {}, {}
                 token_cons_dict, entity_cons_dict = {}, {}
@@ -210,8 +280,8 @@ def main():
                     words = data_inst['str_words']
                     labels = data_inst['tags']
                     
-                    token_freq_dict, token_cons_dict = token_frequency(token_freq_dict, token_cons_dict, words, labels)
-                    entity_freq_dict, entity_density_list, entity_cons_dict = entity_frequency(entity_freq_dict, entity_density_list, entity_cons_dict, words, labels)
+                    token_freq_dict, token_cons_dict = token_frequency(token_freq_dict, token_cons_dict, words, labels, entity_name)
+                    entity_freq_dict, entity_density_list, entity_cons_dict = entity_frequency(entity_freq_dict, entity_density_list, entity_cons_dict, words, labels, entity_name)
                     doc_len = document_length(doc_len, words)
 
                 entity_len_dict = entity_length(entity_freq_dict)
@@ -220,13 +290,61 @@ def main():
                 train_entity_freq_dict = copy.deepcopy(entity_freq_dict)
                 train_entity_cons_dict = copy.deepcopy(entity_cons_dict)
                 # train_entity_density_list = copy.deepcopy(entity_density_list)
+                length_cons_dict = length_per_cons(train_entity_freq_dict, train_entity_cons_dict)
 
             if 'test' in file_name:
                 test_data = copy.deepcopy(data)
                 test_entity_freq_dict = copy.deepcopy(entity_freq_dict)
                 test_doc_len = copy.deepcopy(doc_len)
 
-        length_cons_dict = length_per_cons(train_entity_freq_dict, train_entity_cons_dict)
+        token_list = []
+        for token,token_freq in token_freq_dict.items():
+            token_list.append(token_freq)
+        print ("Token Frequency %.4f" % (np.mean(token_list)))
+
+        entity_list = []
+        for entity, entity_freq in entity_freq_dict.items():
+            entity_list.append(entity_freq)
+        print ("Entity Frequency %.4f" % (np.mean(entity_list)))
+
+        token_o_list = []
+        token_dise_list = []
+        token_chem_list = []
+        for token, token_cons_val in token_cons_dict.items():
+            idx_token_o_list = []
+            idx_token_ent_list = []
+            idx_token_ent2_list = []
+            for token_cons_val_idx in token_cons_val:
+                idx_token_o_list.append(token_cons_val_idx[0])
+                idx_token_ent_list.append(token_cons_val_idx[1])
+                if 'bc5cdr' == entity_name:
+                    idx_token_ent2_list.append(token_cons_val_idx[2])
+            
+            token_o_list.append(np.mean(idx_token_o_list))
+            token_dise_list.append(np.mean(idx_token_ent_list))
+            if 'bc5cdr' == entity_name:
+                token_chem_list.append(np.mean(idx_token_ent2_list))
+
+        print ("Token Consistency O label %.4f"%(np.mean(token_o_list)))
+        print ("Token Consistency First Entity label %.4f"%(np.mean(token_dise_list)))
+        if 'bc5cdr' == entity_name:
+            print ("Token Consistency Second Entity label %.4f"%(np.mean(token_chem_list)))
+            
+        entity_cons_list = []
+        for entity, entity_cons in entity_cons_dict.items():
+            first_entity_cons_list = []
+            for entity_cons_idx in entity_cons:
+                first_entity_cons_list.append(entity_cons_idx)
+
+            entity_cons_list.append(np.mean(first_entity_cons_list))
+            
+        print ("Entity Consistency O Label %.4f" % (1 - np.mean(entity_cons_list)))
+        print ("Entity Consisteny of Entity Label %.4f" % (np.mean(entity_cons_list)))
+        
+        entity_length_list = []
+        for entity, entity_len in entity_len_dict.items():
+            entity_length_list.append(entity_len)
+        print ("Entity Length %.4f" % (np.mean(entity_length_list)))
 
         # dens_cons_dict = dens_per_cons(train_entity_freq_dict, train_entity_density_list)
 
